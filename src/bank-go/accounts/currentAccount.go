e6dc80568f353afc2af86ced31f71967fd136007
Jhosef Marks de Carvalho <jhosef@gmail.com>
2022-05-15 20:30:51 -0300

    bank-go: move CurrentAccount to own file

---
+++ src/bank-go/accounts/currentAccount.go
@@ -0,0 +1,42 @@
+package accounts
+
+type CurrentAccount struct {
+	Holder    string
+	BranchNo  int
+	AccountNo int
+	Balance   float64
+}
+
+func (c *CurrentAccount) Withdraw(value float64) string {
+	validOperation := value > 0 && value <= c.Balance
+
+	if validOperation {
+		c.Balance -= value
+		return "Withdrawal successful."
+	}
+
+	return "Insufficient funds."
+}
+
+func (c *CurrentAccount) Deposit(value float64) string {
+	if value <= 0 {
+		return "Invalid value."
+	}
+
+	c.Balance += value
+
+	return "Deposit successfully."
+}
+
+func (c *CurrentAccount) Tranfer(value float64, destAccount *CurrentAccount) bool {
+	validOperation := value < c.Balance && value > 0
+
+	if validOperation {
+		c.Balance -= value
+		destAccount.Deposit(value)
+
+		return true
+	}
+
+	return false
+}
--- src/bank-go/bankGo.go
+++ src/bank-go/bankGo.go
@@ -1,91 +1,54 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
 
-type CurrentAccount struct {
-	holder    string
-	branchNo  int
-	accountNo int
-	balance   float64
-}
-
-func (c *CurrentAccount) Withdraw(value float64) string {
-	validOperation := value > 0 && value <= c.balance
-
-	if validOperation {
-		c.balance -= value
-		return "Withdrawal successful."
-	}
-
-	return "Insufficient funds."
-}
-
-func (c *CurrentAccount) Deposit(value float64) string {
-	if value <= 0 {
-		return "Invalid value."
-	}
-
-	c.balance += value
-
-	return "Deposit successfully."
-}
-
-func (c *CurrentAccount) Tranfer(value float64, destAccount *CurrentAccount) bool {
-	validOperation := value < c.balance && value > 0
-
-	if validOperation {
-		c.balance -= value
-		destAccount.Deposit(value)
-
-		return true
-	}
-
-	return false
-}
+	"bank-go/accounts"
+)
 
 func main() {
 	showTitle()
 
-	robertAccount := CurrentAccount{
-		holder:    "Robert Griesemer",
-		branchNo:  589,
-		accountNo: 123456,
-		balance:   2300.,
+	robertAccount := accounts.CurrentAccount{
+		Holder:    "Robert Griesemer",
+		BranchNo:  589,
+		AccountNo: 123456,
+		Balance:   2300.,
 	}
 
-	kenAccount := CurrentAccount{
-		holder:    "Ken Thompson",
-		branchNo:  324,
-		accountNo: 341866,
-		balance:   2500,
+	kenAccount := accounts.CurrentAccount{
+		Holder:    "Ken Thompson",
+		BranchNo:  324,
+		AccountNo: 341866,
+		Balance:   2500,
 	}
 
-	fmt.Println(robertAccount.holder, "bank statement")
-	fmt.Println("= Balance.: $", robertAccount.balance)
+	fmt.Println(robertAccount.Holder, "bank statement")
+	fmt.Println("= Balance.: $", robertAccount.Balance)
 	fmt.Println("- Withdraw: $ 1000 -", robertAccount.Withdraw(1000))
-	fmt.Println("= Balance.: $", robertAccount.balance)
+	fmt.Println("= Balance.: $", robertAccount.Balance)
 	fmt.Println("- Withdraw: $ 1500 -", robertAccount.Withdraw(1500))
-	fmt.Println("= Balance.: $", robertAccount.balance)
+	fmt.Println("= Balance.: $", robertAccount.Balance)
 	fmt.Println("- Deposit.: $ 500 -", robertAccount.Deposit(500))
-	fmt.Println("= Balance.: $", robertAccount.balance)
+	fmt.Println("= Balance.: $", robertAccount.Balance)
 	fmt.Println("- Deposit.: $ -50 -", robertAccount.Deposit(-50))
-	fmt.Println("= Balance.: $", robertAccount.balance)
+	fmt.Println("= Balance.: $", robertAccount.Balance)
 
 	fmt.Println()
-	fmt.Println(kenAccount.holder, "bank statement")
-	fmt.Println("= Balance.: $", kenAccount.balance)
+	fmt.Println(kenAccount.Holder, "bank statement")
+	fmt.Println("= Balance.: $", kenAccount.Balance)
 	fmt.Println("- Withdraw: $ 1000 -", kenAccount.Withdraw(1000))
-	fmt.Println("= Balance.: $", kenAccount.balance)
+	fmt.Println("= Balance.: $", kenAccount.Balance)
 	fmt.Println("- Deposit.: $ 700 -", kenAccount.Deposit(700))
-	fmt.Println("= Balance.: $", kenAccount.balance)
+	fmt.Println("= Balance.: $", kenAccount.Balance)
 	fmt.Println("- Transfer: $ 500 -", kenAccount.Tranfer(500, &robertAccount))
-	fmt.Println("= Balance.: $", kenAccount.balance)
+	fmt.Println("= Balance.: $", kenAccount.Balance)
 	fmt.Println("- Withdraw: $ 1500 -", kenAccount.Withdraw(1500))
-	fmt.Println("= Balance.: $", kenAccount.balance)
+	fmt.Println("= Balance.: $", kenAccount.Balance)
 
 	fmt.Println()
-	fmt.Println(robertAccount.holder, "bank statement")
-	fmt.Println("= Balance.: $", robertAccount.balance)
+	fmt.Println(robertAccount.Holder, "bank statement")
+	fmt.Println("= Balance.: $", robertAccount.Balance)
 }
 
 func showTitle() {
