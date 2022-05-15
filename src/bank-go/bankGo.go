093924affc6e0cc20e15436c65989e500e309d01
Jhosef Marks de Carvalho <jhosef@gmail.com>
2022-05-15 19:07:31 -0300

    bank-go: add struct CurrentAccount

--- src/bank-go/bankGo.go
+++ src/bank-go/bankGo.go
@@ -2,11 +2,36 @@ package main
 
 import "fmt"
 
+type CurrentAccount struct {
+	holder    string
+	branchNo  int
+	accountNo int
+	balance   float64
+}
+
 func main() {
 	showTitle()
+
+	robertAccount := CurrentAccount{
+		holder:    "Robert Griesemer",
+		branchNo:  589,
+		accountNo: 123456,
+		balance:   2300.,
+	}
+
+	kenAccount := CurrentAccount{
+		holder:    "Ken Thompson",
+		branchNo:  324,
+		accountNo: 341866,
+		balance:   2500,
+	}
+
+	fmt.Println(robertAccount)
+	fmt.Println(kenAccount)
 }
 
 func showTitle() {
 	fmt.Println("*** Bank GO ***")
 	fmt.Println("*** Welcome ***")
+	fmt.Println()
 }
