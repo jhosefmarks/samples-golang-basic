6a69a93958f9b65c85741d0f3201e1ac3515e130
Jhosef Marks de Carvalho <jhosef@gmail.com>
2022-05-15 22:36:14 -0300

    http-server: add product list

--- src/http-server/main.go
+++ src/http-server/main.go
@@ -6,6 +6,13 @@ import (
 	"net/http"
 )
 
+type Product struct {
+	Name        string
+	Description string
+	Price       float64
+	Amount      int
+}
+
 var temp = template.Must(template.ParseGlob("templates/*.html"))
 
 func main() {
@@ -16,5 +23,12 @@ func main() {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	temp.ExecuteTemplate(w, "Index", nil)
+	products := []Product{
+		{Name: "Blue T-shirt", Description: "Beautiful T-shirt", Price: 39.9, Amount: 5},
+		{"Shoes", "Comfortable shoes for hiking.", 89.9, 3},
+		{"Phone", "The best phone for you.", 259.99, 2},
+		{"Jeans", "Casual clothes for everyday life.", 29.99, 1},
+	}
+
+	temp.ExecuteTemplate(w, "Index", products)
 }
