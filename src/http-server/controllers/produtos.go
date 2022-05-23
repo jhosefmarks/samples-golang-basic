bd5c3c7366c59d0360227dfae3474b4019ac4024
Jhosef Marks de Carvalho <jhosef@gmail.com>
2022-05-22 23:57:10 -0300

    http-server: add GetAllProducts

--- src/http-server/controllers/produtos.go
+++ src/http-server/controllers/produtos.go
@@ -4,45 +4,12 @@ import (
 	"html/template"
 	"net/http"
 
-	"http-server/db"
 	"http-server/models"
 )
 
 var temp = template.Must(template.ParseGlob("templates/*.html"))
 
 func Index(w http.ResponseWriter, r *http.Request) {
-	db := db.ConnectDB()
-
-	rows, err := db.Query(`
-		SELECT ID, NAME, DESCRIPTION, PRICE, AMOUNT
-		FROM PRODUCTS
-		ORDER BY NAME
-	`)
-	if err != nil {
-		panic(err.Error())
-	}
-
-	p := models.Product{}
-	products := []models.Product{}
-
-	for rows.Next() {
-		var id, amount int
-		var name, description string
-		var price float64
-
-		err = rows.Scan(&id, &name, &description, &price, &amount)
-		if err != nil {
-			panic(err.Error())
-		}
-
-		p.Name = name
-		p.Description = description
-		p.Price = price
-		p.Amount = amount
-
-		products = append(products, p)
-	}
-
+	products := models.GetAllProducts()
 	temp.ExecuteTemplate(w, "Index", products)
-	defer db.Close()
 }
--- src/http-server/models/products.go
+++ src/http-server/models/products.go
@@ -1,9 +1,50 @@
 package models
 
+import (
+	"http-server/db"
+)
+
 type Product struct {
 	Id          int
 	Name        string
 	Description string
 	Price       float64
 	Amount      int
+}
+
+func GetAllProducts() []Product {
+	db := db.ConnectDB()
+
+	rows, err := db.Query(`
+		SELECT ID, NAME, DESCRIPTION, PRICE, AMOUNT
+		FROM PRODUCTS
+		ORDER BY NAME
+	`)
+	if err != nil {
+		panic(err.Error())
+	}
+
+	p := Product{}
+	products := []Product{}
+
+	for rows.Next() {
+		var id, amount int
+		var name, description string
+		var price float64
+
+		err = rows.Scan(&id, &name, &description, &price, &amount)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		p.Name = name
+		p.Description = description
+		p.Price = price
+		p.Amount = amount
+
+		products = append(products, p)
+	}
+
+	defer db.Close()
+	return products
 }
