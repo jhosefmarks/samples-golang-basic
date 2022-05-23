e01b05a63d0d4d5d2ebe20086f15fac9f4a71320
Jhosef Marks de Carvalho <jhosef@gmail.com>
2022-05-22 23:44:56 -0300

    http-server: move Index to own file

--- src/http-server/main.go
+++ src/http-server/main.go
@@ -2,56 +2,15 @@ package main
 
 import (
 	"fmt"
-	"html/template"
 	"net/http"
 
-	"http-server/db"
-	"http-server/models"
+	"http-server/controllers"
 )
 
-var temp = template.Must(template.ParseGlob("templates/*.html"))
-
 func main() {
-	http.HandleFunc("/", index)
+	http.HandleFunc("/", controllers.Index)
 
 	fmt.Println("Server listen on port 8000")
 	http.ListenAndServe(":8000", nil)
 
-}
-
-func index(w http.ResponseWriter, r *http.Request) {
-	db := db.ConnectDB()
-
-	rows, err := db.Query(`
-		SELECT ID, NAME, DESCRIPTION, PRICE, AMOUNT
-		FROM PRODUCTS
-		ORDER BY NAME
-	`)
-	if err != nil {
-		panic(err.Error())
-	}
-
-	p := models.Product{}
-	products := []models.Product{}
-
-	for rows.Next() {
-		var id, amount int
-		var name, description string
-		var price float64
-
-		err = rows.Scan(&id, &name, &description, &price, &amount)
-		if err != nil {
-			panic(err.Error())
-		}
-
-		p.Name = name
-		p.Description = description
-		p.Price = price
-		p.Amount = amount
-
-		products = append(products, p)
-	}
-
-	temp.ExecuteTemplate(w, "Index", products)
-	defer db.Close()
 }
